2d9272141f5a6f9dde84e5e06f2b4deaf07e2ac4
kartnik <niktonevidel4@gmail.com>
2024-12-24 23:25:02 +0300

    fileds add

---
+++ products/models.py
@@ -0,0 +1,42 @@
+from django.db import models
+
+from users.models import User
+# Create your models here.
+class ProductCategory(models.Model):
+    name = models.CharField(max_length=20)
+    description = models.TextField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+class Product(models.Model):
+    name=models.CharField(max_length=50)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    quantity = models.PositiveIntegerField(default=0)
+    image= models.ImageField(upload_to='products_images')
+    category= models.ForeignKey(to=ProductCategory, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'Продукт: {self.name} | Категория: {self.category}'
+
+class BasketQuerySet(models.QuerySet):
+    def total_sum(self):
+        return sum(basket.sum() for basket in self)
+
+    def total_quantity(self):
+        return sum(basket.quantity for basket in self)
+
+class Basket(models.Model):
+    user = models.ForeignKey(to=User, on_delete=models.CASCADE)
+    product = models.ForeignKey(to=Product, on_delete=models.CASCADE)
+    quantity = models.PositiveSmallIntegerField(default=0)
+    created_timestamp = models.DateTimeField(auto_now_add=True)
+    objects= BasketQuerySet.as_manager()
+
+    def __str__(self):
+        return f'Корзина для {self.user.email} | Продукт: {self.product.name}'
+
+
+    def sum(self):
+        return self.product.price * self.quantity
